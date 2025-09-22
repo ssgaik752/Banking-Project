ab3dddcbcc8a6518c03894323231d14c24579d8b
ssgaik752 <127913640+ssgaik752@users.noreply.github.com>
2025-09-22 22:58:21 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_eks_cluster" "banking_cluster" {
+  name     = "banking-eks"
+  role_arn = aws_iam_role.eks_role.arn
+  ...
+}
+
+resource "aws_s3_bucket" "banking_data" {
+  bucket = "banking-app-data"
+  acl    = "private"
+}
 
